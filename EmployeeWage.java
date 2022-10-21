cba7c43630cdde94c4977ccf471efd5a55262169
Arun93516 <arunkumar93516@gmail.com>
2022-10-21 19:07:21 +0530

    Added:Welcome to Employee Wage computation Problem UC2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,18 +1,25 @@
 package Day10Assignment;
+
 import java.util.Scanner;
 import java.math.*;
+
 public class EmployeeWage {
-		public static void main(String[] args) {
-			Scanner in = new Scanner(System.in);
-			System.out.println("Welcome to Employee Wage Computation");
-			int randomCheck = (int) (Math.random() * 3);
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Welcome to Employee Wage Computation");
+		int randomCheck = (int) (Math.random() * 3);
+		int Wage_per_Hour = 20;
+		int Full_Day_Hour = 8;
+		int Total_Wage = 0;
 
-			if (randomCheck == 0) {
-				System.out.println("Employee is absent");
-			} 
-			else if (randomCheck == 1) {
-				System.out.println("Employee is present");
-			}
+		if (randomCheck == 0) {
+			System.out.println("Employee is absent");
+		} else if (randomCheck == 1) {
+			Total_Wage = Wage_per_Hour * Full_Day_Hour;
+
+			System.out.println("Employee is present");
+			System.out.println(Total_Wage);
 		}
-	}
 
+	}
+}
