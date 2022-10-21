139be84b7724574ec81a07f472cc1c43c219996f
Arun93516 <arunkumar93516@gmail.com>
2022-10-21 19:33:43 +0530

    Added:Welcome to Employee Wage computation Problem Uc4 Using Switch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,20 +7,29 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Welcome to Employee Wage Computation");
-		int randomCheck = (int) (Math.random() * 3);
+		int status = (int) (Math.random()* 3);
 		int Wage_per_Hour = 20;
 		int Full_Day_Hour = 8;
-		int Off_Day_Hour = 4;
+		int Part_Time_Hour = 4;
 		int Total_Wage = 0;
 
-		if (randomCheck == 0) {
+		switch (status) {
+		case 0: {
 			System.out.println("Employee is absent");
-		} else if (randomCheck == 1) {
+			break;
+			
+		}
+		case 1: {
+			System.out.println("Part time Hour");
+			Total_Wage = Wage_per_Hour * Part_Time_Hour;
+			break;
+			
+		}
+		default: {
 			Total_Wage = Wage_per_Hour * Full_Day_Hour;
-
 			System.out.println("Employee is present");
 			System.out.println(Total_Wage);
 		}
-
+		}
 	}
 }
